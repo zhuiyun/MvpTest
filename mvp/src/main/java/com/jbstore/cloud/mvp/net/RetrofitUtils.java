5cd39282740ff9e28d08aa6bb339d81c6ae547a3
zhuiyun <zhuiyun0413@163.com>
2018-01-23 18:15:16 +0800

    add update

--- mvp/src/main/java/com/jbstore/cloud/mvp/net/RetrofitUtils.java
+++ mvp/src/main/java/com/jbstore/cloud/mvp/net/RetrofitUtils.java
@@ -20,4 +20,6 @@ public class RetrofitUtils {
         return api;
     }
 
+
+
 }
--- mvp/src/main/java/com/jbstore/cloud/mvp/presenter/WeatherPresenter.java
+++ mvp/src/main/java/com/jbstore/cloud/mvp/presenter/WeatherPresenter.java
@@ -8,9 +8,7 @@ import com.jbstore.cloud.mvp.net.RetrofitUtils;
 import com.jbstore.cloud.mvp.view.WeatherView;
 
 import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by gwy on 2018/1/23.
@@ -32,27 +30,25 @@ public class WeatherPresenter extends IPresenter<WeatherView> {
     }
 
     public void getData(String city) {
-        RetrofitUtils.getRequest().getCall().observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(new Observer<JsonObject>() {
+        RetrofitUtils.getRequest().getCall().subscribe(new Observer<JsonObject>() {
             @Override
             public void onSubscribe(Disposable d) {
-                ((WeatherView) getView()).showLoading();
+                Log.e("gao", "onSubscribe: ");
             }
 
             @Override
             public void onNext(JsonObject value) {
                 Log.e("gao", "onNext: "+value);
-//                getView().setData(value);
             }
 
             @Override
             public void onError(Throwable e) {
                 Log.e("gao", "onError: "+e.toString());
-
             }
 
             @Override
             public void onComplete() {
-                getView().hideLoading();
+                Log.e("gao", "onComplete: ");
             }
         });
     }
